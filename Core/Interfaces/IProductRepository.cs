b91adb09f678ea001c4478a748cacd606121d231
Harsh Mendapara <patelharshh.hp@gmail.com>
2022-05-20 10:40:11 +0530

    #11: Created generic repository and interface.

--- Core/Interfaces/IProductRepository.cs
+++ Core/Interfaces/IProductRepository.cs
@@ -6,10 +6,10 @@ namespace Core.Interfaces
 {
   public interface IProductRepository
   {
-    Task<Product> GetProductByIdAsync(int id);
-    Task<IReadOnlyList<Product>> GetProductAsync();
+     Task<Product> GetProductByIdAsync(int id);
+     Task<IReadOnlyList<Product>> GetProductAsync();
      Task<IReadOnlyList<ProductBrand>> GetProductBrandsAsync();
-      Task<IReadOnlyList<ProductType>> GetProductTypesAsync();
+     Task<IReadOnlyList<ProductType>> GetProductTypesAsync();
 
   }
 }
