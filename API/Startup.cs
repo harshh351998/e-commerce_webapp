131b90d1fdcb88ecbed47f0751180ae0bebf8902
harshh351998 <mendapara.harsh47@gmail.com>
2022-05-27 09:24:25 +0530

    #25: Exception handler of middleware created.

--- API/Startup.cs
+++ API/Startup.cs
@@ -3,6 +3,7 @@ using Infrastructure.Data;
 using Core.Interfaces;
 using AutoMapper;
 using API.Helpers;
+using API.Middleware;
 
 namespace API
 {
@@ -27,9 +28,11 @@ namespace API
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseMiddleware<ExceptionMiddleware>();
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
                 
             }
             
