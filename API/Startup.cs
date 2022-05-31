a60a09efe7cfa1de998ff8c9a822800207cdf364
Harsh Mendapara <mendapara.harsh47@gmail.com>
2022-05-31 16:55:14 +0530

    #36: Cors support policy added.

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,35 +1,31 @@
 using Microsoft.EntityFrameworkCore;
 using Infrastructure.Data;
-using Core.Interfaces;
-using AutoMapper;
 using API.Helpers;
 using API.Middleware;
-using System.Linq;
-using Microsoft.AspNetCore.Mvc;
-using API.Errors;
-using Microsoft.OpenApi.Models;
 using API.Extensions;
 
 namespace API
 {
-    public class Startup
+  public class Startup
     {
         private readonly IConfiguration _config;
         public Startup(IConfiguration _config) => this._config = _config;
 
-
-
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            
             services.AddAutoMapper(typeof(MappingProfiles));
             services.AddControllers();
             services.AddDbContext<StoreContext>(x => x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
-
             services.AddApplicationServices();
             services.AddSwaggerDocumentation();
-            
+            services.AddCors( opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:7227");
+                });
+            });
             
         }
 
@@ -37,19 +33,13 @@ namespace API
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             app.UseMiddleware<ExceptionMiddleware>();
-           
             app.UseStatusCodePagesWithReExecute("/errors/{0}");
-
             app.UseHttpsRedirection();
-
             app.UseRouting();
-
             app.UseStaticFiles();
-
+            app.UseCors("CorsPolicy");
             app.UseAuthorization();
-            
             app.UseSwaggerDocumentation();
-
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
