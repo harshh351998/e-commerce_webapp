0e3ef5eb7f61a85853e09b726099ad9ad8fe9438
Harsh Mendapara <mendapara.harsh47@gmail.com>
2022-06-01 15:43:53 +0530

    #40: Http client module added.

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit {
+  title = 'shoppershut';
+  products: any[];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:7227/api/products?pageSize=50').subscribe((response: any)=>{
+      this.products = response.data;
+    }, error =>{
+      console.log(error);
+    });
+  }
 }
