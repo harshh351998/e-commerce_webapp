5c87695adef524c66c2f78d7b23410ce2095979a
Harsh Mendapara <mendapara.harsh47@gmail.com>
2022-06-02 12:40:51 +0530

    #48:- Product filters data added.

--- client/src/app/shop/shop.component.ts
+++ client/src/app/shop/shop.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { IBrand } from '../shared/models/brand';
 import { IProduct } from '../shared/models/product';
+import { IType } from '../shared/models/ProductType';
 import { ShopService } from './shop.service';
 
 @Component({
@@ -9,13 +11,41 @@ import { ShopService } from './shop.service';
 })
 export class ShopComponent implements OnInit {
   products: IProduct[];
+  brands: IBrand[];
+  types: IType[];
 
   // Injecting shop service
   constructor(private shopService: ShopService) { }
 
   ngOnInit(): void {
+    this.getProducts();
+    this.getBrands();
+    this.getTypes();
+  }
+
+  // Method for getting products
+  getProducts(){
     this.shopService.getProducts().subscribe(response => {
       this.products = response.data;
+    }, error =>{
+      console.log(error);
+    });
+
+  }
+
+  //Method for getting product brands
+  getBrands(){
+    this.shopService.getBrands().subscribe(response => {
+      this.brands = response;
+    }, error =>{
+      console.log(error);
+    });
+  }
+
+  //Method for getting product types
+  getTypes(){
+    this.shopService.getTypes().subscribe(response => {
+      this.types = response;
     }, error =>{
       console.log(error);
     });
--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { IBrand } from '../shared/models/brand';
 import { IPagination } from '../shared/models/pagination';
+import { IType } from '../shared/models/ProductType';
 
 @Injectable({
   providedIn: 'root'
@@ -13,5 +15,15 @@ export class ShopService {
   //Method for getting products
   getProducts(){
     return this.http.get<IPagination>(this.baseUrl + 'products');
+  }
+
+  //Method for getting product brands
+  getBrands(){
+    return this.http.get<IBrand[]>(this.baseUrl + 'products/brands');
+  }
+
+  //Method for getting product types
+  getTypes(){
+    return this.http.get<IType[]>(this.baseUrl + 'products/types');
   }
 }
