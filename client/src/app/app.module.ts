cf08550a5f4c778a8a4ed89f57e4144ac442c01c
Harsh Mendapara <mendapara.harsh47@gmail.com>
2022-06-03 14:26:31 +0530

    #63:- Lazing loading of shop module added.

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -7,7 +7,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CoreModule } from './core/core.module';
-import { ShopModule } from './shop/shop.module';
+
 
 
 @NgModule({
@@ -21,7 +21,6 @@ import { ShopModule } from './shop/shop.module';
     BrowserAnimationsModule,
     HttpClientModule,
     CoreModule,
-    ShopModule,
     HomeModule
   ],
   providers: [],
--- client/src/app/shop/shop.module.ts
+++ client/src/app/shop/shop.module.ts
@@ -1,11 +1,10 @@
+import { ShopRoutingModule } from './shop-routing.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ShopComponent } from './shop.component';
 import { ProductItemComponent } from './product-item/product-item.component';
 import { SharedModule } from '../shared/shared.module';
 import { ProductDetailsComponent } from './product-details/product-details.component';
-import { RouterModule } from '@angular/router';
-
 
 
 @NgModule({
@@ -17,8 +16,8 @@ import { RouterModule } from '@angular/router';
   imports: [
     CommonModule,
     SharedModule,
-    RouterModule
-  ],
-  exports: [ShopComponent]
+    ShopRoutingModule
+  ]
+
 })
 export class ShopModule { }
