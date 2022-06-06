6be16b578c330d8488d39ebfc481de3647f09763
Harsh Mendapara <mendapara.harsh47@gmail.com>
2022-06-06 08:31:24 +0530

    #64:- Error-tests component created.

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
+import { TestErrorComponent } from './core/test-error/test-error.component';
 import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ShopComponent } from './shop/shop.component';
-import { ProductDetailsComponent } from './shop/product-details/product-details.component';
 
 const routes: Routes = [
   {path:'', component: HomeComponent},
+  {path:'test-error', component: TestErrorComponent},
   {path:'shop',loadChildren: () => import('./shop/shop.module').then(mod => mod.ShopModule)}, 
   {path:'**',redirectTo: '',pathMatch: 'full'}
 ];
