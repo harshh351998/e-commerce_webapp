49e645f4aef68b457febf9a97cbd066bbd443901
Harsh Mendapara <mendapara.harsh47@gmail.com>
2022-06-07 13:51:37 +0530

    #73:- Redis setup.

--- API/Startup.cs
+++ API/Startup.cs
@@ -3,6 +3,7 @@ using Infrastructure.Data;
 using API.Helpers;
 using API.Middleware;
 using API.Extensions;
+using StackExchange.Redis;
 
 namespace API
 {
@@ -16,7 +17,15 @@ namespace API
         {
             services.AddAutoMapper(typeof(MappingProfiles));
             services.AddControllers();
-            services.AddDbContext<StoreContext>(x => x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
+            services.AddDbContext<StoreContext>(x => x.UseSqlite(_config
+                .GetConnectionString("DefaultConnection")));
+                
+            services.AddSingleton<ConnectionMultiplexer>(c => {
+                var configuration = ConfigurationOptions.Parse(_config
+                .GetConnectionString("Redis"),
+                true);
+                return ConnectionMultiplexer.Connect(configuration);
+            });
             services.AddApplicationServices();
             services.AddSwaggerDocumentation();
             services.AddCors( opt =>
