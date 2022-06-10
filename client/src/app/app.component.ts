1408e050531ee70cacf63ad7776e3ab31ae2fd26
Harsh Mendapara <patelharshh.hp@gmail.com>
2022-06-10 15:38:47 +0530

    #78:Persisting the basket on startup.

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { BasketService } from './basket/basket.service';
 import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-root',
@@ -7,7 +8,15 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
   title = 'shoppershut';
  
-  constructor() {}
+  constructor(private basketService: BasketService) {}
   ngOnInit(): void {
+    const basketId = localStorage.getItem('basket_id');
+    if (basketId) {
+      this.basketService.getBasket(basketId).subscribe(() =>{
+        console.log('initialised basekt');
+      }, error => {
+        console.log(error);
+      });
+    }
   }
 }
