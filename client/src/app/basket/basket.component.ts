a1d28268d5b907c72744a7cf6860c17ffb0186a0
Harsh Mendapara <mendapara.harsh47@gmail.com>
2022-06-13 08:59:09 +0530

    #79: Cart item count number added.

--- client/src/app/basket/basket.component.ts
+++ client/src/app/basket/basket.component.ts
@@ -1,3 +1,6 @@
+import { BasketService } from './basket.service';
+import { IBasket } from './../shared/models/basket';
+import { Observable } from 'rxjs';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +9,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./basket.component.scss']
 })
 export class BasketComponent implements OnInit {
+  basket$: Observable<IBasket>;
 
-  constructor() { }
+  constructor(private basketService: BasketService) { }
 
   ngOnInit(): void {
+    this.basket$ = this.basketService.basket$;
   }
 
 }
